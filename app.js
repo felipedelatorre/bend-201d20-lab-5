adce5b8e4028e1434c9b8835b9d306a092ad6212
Chacs <felipede_latorre@hotmail.com>
2019-03-16 21:05:43 -0700

    solve multiplyarray

--- app.js
+++ app.js
@@ -121,11 +121,20 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  var arrayForProblemFive = [];
+  var productForMultiplyArray = 0;
 
+  productForMultiplyArray = multiply(multArr[0], multArr[1])[0];
+  productForMultiplyArray = multiply(productForMultiplyArray, multArr[2])[0];
+
+  arrayForProblemFive.push(productForMultiplyArray);
+  arrayForProblemFive.push('The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + arrayForProblemFive[0] + '.');
+  
+  return arrayForProblemFive;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
